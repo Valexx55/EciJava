77150fce3339962d72100175e7a4d44abcff0a06
Vale <valexx55@gmail.com>
2022-06-06 11:38:54 +0200

    v1

---
+++ ejercicio1_profe/src/ejercicio1/Prueba.java
@@ -0,0 +1,24 @@
+package ejercicio1;
+
+/**
+ * 
+ * @author USUARIO
+ *
+ */
+public class Prueba {
+
+	public  int calcula ()
+	{
+	return 1+1;
+	}
+	/**
+	 * 
+	 * @param argumentos
+	 */
+	public static void main (String [] argumentos)
+	{
+		Prueba prueba = new Prueba();
+		System.out.println (prueba.calcula());
+	}
+}
+
---
+++ ejercicio1_profe/src/module-info.java
@@ -0,0 +1,2 @@
+module ejercicio1_profe {
+}
