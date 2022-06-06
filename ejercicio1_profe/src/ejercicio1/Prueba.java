c14bbd3e1d56c4321176def26f20226b488c0204
Vale <valexx55@gmail.com>
2022-06-06 22:41:39 +0200

    gitignore metadata

--- ejercicio1_profe/src/ejercicio1/Prueba.java
+++ ejercicio1_profe/src/ejercicio1/Prueba.java
@@ -9,6 +9,10 @@ public class Prueba {
 
 	public  int calcula ()
 	{
+		int num = 0;
+		Integer numero = 8;
+		//numero.
+				
 	return 1+1;
 	}
 	/**
@@ -19,6 +23,12 @@ public class Prueba {
 	{
 		Prueba prueba = new Prueba();
 		System.out.println (prueba.calcula());
+		
+		for (int a=0; (a%6)<6; a++)
+		{
+			System.out.println (a);
+		}
+
 	}
 }
 
