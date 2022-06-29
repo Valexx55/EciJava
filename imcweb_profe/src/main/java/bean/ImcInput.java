4a9ace9f49f38a2d1466627390a36c4003a75832
Vale <valexx55@gmail.com>
2022-06-29 09:40:51 +0200

    adjunto...se me olvidaba

--- imcweb_profe/src/main/java/bean/ImcInput.java
+++ imcweb_profe/src/main/java/bean/ImcInput.java
@@ -1,5 +1,57 @@
 package bean;
 
 public class ImcInput {
+	
+	private String nombre;
+	private int id;
+	private float estatura;
+	private float peso;
+	
+	public ImcInput() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public ImcInput(String nombre, int id, float estatura, float peso) {
+		super();
+		this.nombre = nombre;
+		this.id = id;
+		this.estatura = estatura;
+		this.peso = peso;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "ImcInput [nombre=" + nombre + ", id=" + id + ", estatura=" + estatura + ", peso=" + peso + "]";
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public float getEstatura() {
+		return estatura;
+	}
+	public void setEstatura(float estatura) {
+		this.estatura = estatura;
+	}
+	public float getPeso() {
+		return peso;
+	}
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+	
+	
 
 }
